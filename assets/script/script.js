3932b7d5bf6fc65995df8ff7bf9b45c3422ea91f
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-02-05 12:51:56 +0200

    add counter

--- assets/script/script.js
+++ assets/script/script.js
@@ -132,49 +132,76 @@ tabsBtns.forEach((button) => {
         });
     });
     
-    let stat = document.getElementsByClassName('stat-value');
+    function windowLoad() {
 
-let zeroValues = () => {
-  for (let i = 0; i < stat.length; i++) {
-    stat[i].innerHTML = 0;
-  }
-}
-
-let numScroll = () => {
-  const animationDuration = 2000;
-  const frameDuration = 1000 / 60;
-  const totalFrames = Math.round(animationDuration / frameDuration);
-  const easeOutQuad = t => t * (2 - t);
-  
-  const animateCountUp = el => {
-    let frame = 0;
-    const countTo = parseInt(el.dataset.target.replace(/,/g, ''), 10);
+      function statValueInit(statValues) {
+        let values = statValues ? statValues : document.querySelectorAll('.stat-value');
+        if (values) {
+          values.forEach(statValue => {
+            numScroll(statValue);
+          })
+        }
+      }
     
-    const counter = setInterval(() => {
-      frame++; 
-      const progress = easeOutQuad(frame / totalFrames);
-      const currentCount = Math.round(countTo * progress);
-      
-      if (parseInt(el.innerHTML, 10) !== currentCount) {
-        el.innerHTML = currentCount;
+      let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const targetElement = entry.target;
+            const statValues = targetElement.querySelectorAll('.stat-value');
+            if (statValues.length) {
+              statValueInit(statValues);
+            }
+          }
+        })
+      }, { threshold: 0.3 });
+    
+      let sections = document.querySelectorAll('.about__container-bottom-item');
+      if (sections.length) {
+        sections.forEach(section => {
+          observer.observe(section);
+        })
       }
-      
-      if (frame === totalFrames) {
-        clearInterval(counter);
-        el.innerHTML = el.dataset.target;
+    
+      function numScroll(statValue) {
+        let zeroValues = () => {
+          statValue.innerHTML = 0;
+        }
+    
+        const animationDuration = 3000;
+        const frameDuration = 1000 / 60;
+        const totalFrames = Math.round(animationDuration / frameDuration);
+        const easeOutQuad = t => t * (2 - t);
+    
+        const animateCountUp = () => {
+          let frame = 0;
+          const countTo = parseInt(statValue.dataset.target.replace(/,/g, ''), 10);
+    
+          const counter = setInterval(() => {
+            frame++;
+            const progress = easeOutQuad(frame / totalFrames);
+            const currentCount = Math.round(countTo * progress);
+    
+            if (parseInt(statValue.innerHTML, 10) !== currentCount) {
+              statValue.innerHTML = currentCount;
+            }
+    
+            if (frame === totalFrames) {
+              clearInterval(counter);
+              statValue.innerHTML = statValue.dataset.target;
+            }
+          }, frameDuration);
+        }
+    
+        const runAnimations = () => {
+          animateCountUp();
+        }
+        runAnimations();
       }
-    }, frameDuration);
-  }
-  
-  const runAnimations = () => {
-    const countupEls = document.querySelectorAll('.stat-value');
-    countupEls.forEach(animateCountUp);
-  }
-  
-  runAnimations();
-}
-
-window.addEventListener('load', () => {
-  zeroValues();
-  numScroll();
-});
+      window.addEventListener('DOMContentLoaded', () => {
+        statValueInit();
+      });
+    
+    }
+    
+    windowLoad();
+    
